f8283382e2d84f859a2a32a04572c8e58658e68a
AbbasFahimi <abbas.fahimy80@gmail.com>
2025-02-05 18:04:39 +0330

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $("#add").click(function () {
+    $(this).before(
+      '<div class="input-group mb-3"><button class="btn-1 hidden input-group-text input-group-text-custom"><i class="fa-solid fa-check"></i></button></i><input placeholder="افزودن کار جدید ..." type="text" class="form-control" aria-label="Dollar amount (with dot and two decimal places)"/><button  class=" btn-2 hidden input-group-text input-group-text-custom"><i class="fa-solid fa-trash"></i></button> <button  class=" btn-3 hidden input-group-text input-group-text-custom"><i class="fa-solid fa-pen-to-square"></i></button></div>'
+    );
+  });
+  $("#submit").click(function () {
+    $(".body div").children("input").attr("disabled", true);
+    $(".body div").children(".btn-2 , .btn-3 , .btn-1").removeClass("hidden");
+  });
+  $(".body").on("click", ".btn-2", function () {
+    $(this).parent(".input-group").fadeOut(200);
+  });
+  $(".body").on("click", ".btn-3", function () {
+    $(this)
+      .parent(".input-group")
+      .children("input")
+      .attr("disabled", false)
+      .focus();
+  });
+  $("#delet").click(function () {
+    $(".input-group").fadeOut(200);
+  });
+  $(".body").on("click", ".btn-1", function () {
+    $(this)
+      .parent(".input-group")
+      .children("input")
+      .css({ "background-color": "#24ad24", color: "#fff" });
+  });
+});
